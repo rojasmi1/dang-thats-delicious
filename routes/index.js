e8c26bd15723a299a2d0c06f961620c8453e0b36
Michael <michael.rojasp@gmail.com>
2017-05-17 19:27:35 -0600

    Add missing initial files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.send({message: 'It Works'});
 });
 
 module.exports = router;
