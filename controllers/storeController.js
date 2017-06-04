7957e68756da88e284b44026dad981d7867067c8
Michael <michael.rojasp@gmail.com>
2017-06-04 16:29:16 -0600

    Create tags view and list all tags by popularity

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -82,3 +82,9 @@ exports.getStoreBySlug = async (req, res, next) => {
   }
   res.render('store', {title: `${store.name}`, store})
 }
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList()
+  const tag = req.params.tag
+  res.render('tags', {tags, title: 'Tags', tag})
+}
--- models/Store.js
+++ models/Store.js
@@ -51,5 +51,12 @@ storeSchema.pre('save', async function (next) {
   // TODO make more resiliant so slugs are unique
 })
 
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    {$unwind: '$tags'},
+    {$group: {_id: '$tags', count: {$sum: 1}}},
+    {$sort: {count: -1}}
+  ])
+}
 
 module.exports = mongoose.model('Store', storeSchema)
